1c00d1e98e31d22097cd0128326a220bf45138c6
ivanlynch <ivan.lynch@outlook.com.ar>
2016-11-06 06:46:00 -0300

    Se creo la API y los methodos para manipular informacion

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,114 @@
-/* Declaramos express y creamos el router*/ 
+/* Declaramos los modulos que vamos a usar en el router*/ 
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
 
-/* Le decimos al router: Hey!, si alguien quiere conocer las tareas envialo a la API */
+/* Declamos nuestra Base de Datos */
+var db = mongojs('mongodb://localhost:27017/tasks', ['tasks']);
+
+/* GET METHOD, Le decimos al router: Hey!, si alguien quiere conocer las tareas envialo a la API */
 router.get('/tasks', function(request, response, next){
-	response.send('TASK API');
+
+	/* Hacemos la query como en Mongo pero le pasamos una funcion como parametro */
+	db.tasks.find(function(error, tasks){
+		if(error){
+			response.send(error);
+		}
+		/* Si encuentra algo, mandamelo como JSON */
+		response.json(tasks);
+	});
+
+});
+
+/* GET METHOD BY ID */
+router.get('/task/:id', function(request, response, next){
+
+/*  Buscamos por Id  -  le pasamos el objeto que viene en los parametros de la peticion */
+	db.tasks.findOne({_id: mongojs.ObjectId(request.params.id)}, function(error, task){
+
+		if(error){
+			response.send(error);
+		}
+		response.json(task);
+	});
+
+});
+
+/* POST METHOD */
+router.post('/tasks', function(request, response, next){
+
+	var task = request.body;
+
+	if(!task.title || (task.estaHecho + '')){
+
+		response.status(400);
+		response.json({
+			"Error" : "No hay información"
+		});
+
+	}else{
+
+		db.tasks.save(task, function(error, task){
+
+			if(error){
+
+				response.send(error);
+			}
+				response.json(tasks);
+		});
+	}
+});
+
+
+/* DELETE METHOD! */
+router.delete('/task/:id', function(request, response, next){
+
+	db.tasks.remove({_id: mongojs.ObjectId(request.params.id)}, function(error, task){
+
+		if(error){
+
+			response.send(error);
+		}
+
+		response.json(task);
+	});
+});
+
+
+/* UPDATE METHOD */
+router.put('/task/:id', function(request, response, next){
+
+	var task = request.body;
+	var updTask = {};
+
+	if(task.estaHecho){
+		updTask.estaHecho = task.estaHecho;
+	}
+
+	if(task.title){
+		updTask.title = task.title;
+	}
+
+	if(!updTask){
+
+		response.status(400);
+		response.json({
+			"Error" : "No hay información"
+		});
+
+	}else{
+
+		db.tasks.update({_id: mongojs.ObjectId(request.params.id)}, updTask, {}, function(error, task){
+
+			if(error){
+				response.send(error);
+			}
+			response.json(task);
+
+		});
+
+	}
+
 });
 
 /* Exportamos el modulo */
