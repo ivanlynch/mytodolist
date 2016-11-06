bca9022bf782a99a7ea572de7989efe49f257596
ivanlynch <ivan.lynch@outlook.com.ar>
2016-11-06 07:58:12 -0300

    Se creo el UI basico para la vista de nuestro Task List

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -11,14 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var app_component_1 = require('./app.component');
+var tasks_component_1 = require('./components/tasks/tasks.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent],
-            bootstrap: [app_component_1.AppComponent]
+            declarations: [app_component_1.AppComponent, tasks_component_1.TasksComponent],
+            bootstrap: [app_component_1.AppComponent, tasks_component_1.TasksComponent]
         }), 
         __metadata('design:paramtypes', [])
     ], AppModule);
